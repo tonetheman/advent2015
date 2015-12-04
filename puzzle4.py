b44aa5c65cbaceae1f99716969aa529af1332ef2
tony <tonetheman@gmail.com>
2015-12-04 08:03:02 -0600

    first part of 4

---
+++ puzzle4.py
@@ -0,0 +1,15 @@
+
+import hashlib
+
+secret_key = "iwrupvqb"
+
+for i in range(10000000):
+	ts = secret_key + str(i)
+
+	m = hashlib.md5()
+	m.update(ts)
+	hd = m.hexdigest()
+	if hd[0:5] == "00000":
+		print i,ts, m.hexdigest()
+		break
+	
