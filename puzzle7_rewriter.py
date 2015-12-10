5cf1c25991eb327e548f48fbc8dc98380c5ec965
tony <tonetheman@gmail.com>
2015-12-09 19:27:48 -0600

    still working on puzzle7

--- puzzle7_rewriter.py
+++ puzzle7_rewriter.py
@@ -9,6 +9,8 @@ PA = re.compile("([0-9a-z]+) -> ([a-z]+)")
 NOTRULE = re.compile("NOT ([a-z]+) -> ([a-z]+)")
 ANDOR = re.compile("([0-9a-z]+) (AND|OR) ([a-z]+) -> ([a-z]+)")
 SHIFT = re.compile("([a-z]+) (LSHIFT|RSHIFT) (\d+) -> ([a-z]+)")
+PREFIX = "_"
+NUM = re.compile("\d+")
 
 def log(*args):
 	ts = ""
@@ -16,34 +18,41 @@ def log(*args):
 		ts = ts + str(a) + " "
 	print ts
 
+def _v(a):
+	m = NUM.match(a)
+	if m is not None:
+		return a
+	else:
+		return PREFIX + a	
+
 def try_assign(line):
 	if PA.match(line):
 		m = PA.match(line)
-		log(m.group(2), "=", m.group(1))
+		log(_v(m.group(2)), "=", _v(m.group(1)))
 		return True
 	return False
 def try_andor(line):
 	if ANDOR.match(line):
 		m = ANDOR.match(line)
 		if m.group(2) == "AND":
-			log(m.group(3), "=", m.group(1), "&", m.group(3))
+			log(_v(m.group(3)), "=", _v(m.group(1)), "&", _v(m.group(3)))
 		elif m.group(2) == "OR":
-			log(m.group(3), "=", m.group(1), "|", m.group(3))
+			log(_v(m.group(3)), "=", _v(m.group(1)), "|", _v(m.group(3)))
 		return True
 	return False
 def try_shift(line):
 	if SHIFT.match(line):
 		m = SHIFT.match(line)
 		if m.group(2) == "LSHIFT":
-			log(m.group(4),"=",m.group(1),"<<",m.group(3))
+			log(_v(m.group(4)),"=", _v(m.group(1)),"<<",_v(m.group(3)))
 		elif m.group(2) == "RSHIFT":
-			log(m.group(4),"=",m.group(1),">>",m.group(3))
+			log(_v(m.group(4)),"=",_v(m.group(1)),">>",_v(m.group(3)))
 		return True
 	return False
 def try_not(line):
 	if NOTRULE.match(line):
 		m = NOTRULE.match(line)
-		log(m.group(2), "=", "~", m.group(1))
+		log(_v(m.group(2)), "=", "~", _v(m.group(1)))
 		return True
 	return False
 
